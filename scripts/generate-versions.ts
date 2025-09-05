4104be6f036e23faab8a29999eca4304e65fb068
DuH Nunes <duhnunes.dev@gmail.com>
2025-09-04 22:46:43 -0300

    fix(build): prevent crash when versioned folder is missing dring generation - Adds check for existence of versioned/ folder - Creates folder and generates empty versions.json if missing - Ensures build doesn't fail on environments like Vercel

--- scripts/generate-versions.ts
+++ scripts/generate-versions.ts
@@ -3,6 +3,17 @@ import fs from 'fs'
 import path from 'node:path'
 
 const versionDir = path.join(process.cwd(), 'versioned')
+const outputPath = path.join(process.cwd(), 'versions.json')
+
+if (!fs.existsSync(versionDir)) {
+  console.warn(
+    'Folder "versioned/" not found. Creating empty folder and version.json.'
+  )
+  fs.mkdirSync(versionDir, { recursive: true })
+  fs.writeFileSync(outputPath, JSON.stringify([], null, 2))
+  process.exit(0)
+}
+
 const folders = fs.readdirSync(versionDir)
 
 const versions = folders
@@ -16,5 +27,4 @@ const versions = folders
       })
   )
 
-const outputPath = path.join(process.cwd(), 'versions.json')
 fs.writeFileSync(outputPath, JSON.stringify(versions, null, 2))
